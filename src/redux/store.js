110d25fa45a86351a1ad75ca06110ef353c007da
Davincy Project <bdavincy.bd@gmail.com>
2023-11-13 19:51:11 +0700

    tweaks: changes store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import rootReducers from "./reducers";
-import searchReducers from "./reducers/searchReducers";
 
 export default configureStore({
   reducer: rootReducers,
-  search: searchReducers,
-  devTools: import.meta.env.MODE === `${import.meta.env.VITE_DEVTOOLS}`,
+  // devTools: import.meta.env.MODE === `${import.meta.env.VITE_DEVTOOLS}`,
+  devTools: import.meta.env.VITE_MODE !== "production",
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
