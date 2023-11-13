91e787c0c155761f7f1b82e6ed1bb59d102aa878
Billy <76769657+DavincyProject@users.noreply.github.com>
2023-11-13 19:52:40 +0700

    Staging (#1)
    * staging commit
    
    * test
    
    * test mode devTools
    
    * testing devtool
    
    * Update store.js
    
    * fix: remove all data from redux after logout
    
    * tweaks: change store to enable devtools
    
    * tweaks: changes store.js
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>

--- src/Components/PopularMovies.jsx
+++ src/Components/PopularMovies.jsx
@@ -4,43 +4,45 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 
 const PopularMovies = () => {
-    const [showAllMovies, setShowAllMovies] = useState(false);
+  const [showAllMovies, setShowAllMovies] = useState(false);
 
-    const totalResults = useSelector((state) => state.popular.totalResult);
+  const totalResults = useSelector((state) => state.popular.totalResult);
 
-    const handleSeeAllClick = () => {
-        setShowAllMovies(true);
-    };
+  const handleSeeAllClick = () => {
+    setShowAllMovies(true);
+  };
 
-    return (
-        <div className="container-fluid mt-3 ">
-            <div className="d-flex justify-content-between align-items-center mx-4 mb-4">
-                {totalResults && (
-                    <>
-                        <h1 className="mb-0 text-light fs-2 ">Popular Movie</h1>
+  return (
+    <div className="container-fluid mt-3 ">
+      <div className="d-flex justify-content-between align-items-center mx-4 mb-4">
+        {totalResults && (
+          <>
+            <h1 className="mb-0 text-light fs-2 ">
+              Popular Movie - Staging ver
+            </h1>
 
-                        <Link
-                            className="text-decoration-none text-danger fw-semibold cursor-pointer"
-                            onClick={handleSeeAllClick}
-                        >
-                            See All Movies{" "}
-                            <span>
-                                <img
-                                    src="/arrow.svg"
-                                    alt="arrow_icon"
-                                    style={{ width: "20px" }}
-                                />
-                            </span>
-                        </Link>
-                    </>
-                )}
-            </div>
+            <Link
+              className="text-decoration-none text-danger fw-semibold cursor-pointer"
+              onClick={handleSeeAllClick}
+            >
+              See All Movies{" "}
+              <span>
+                <img
+                  src="/arrow.svg"
+                  alt="arrow_icon"
+                  style={{ width: "20px" }}
+                />
+              </span>
+            </Link>
+          </>
+        )}
+      </div>
 
-            <div>
-                <PopularCard showAllMovies={showAllMovies} />
-            </div>
-        </div>
-    );
+      <div>
+        <PopularCard showAllMovies={showAllMovies} />
+      </div>
+    </div>
+  );
 };
 
 export default PopularMovies;
